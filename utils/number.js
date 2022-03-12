e5ad407ab6f4be3745939cd47d6a3d70b3ed6f61
Felipe <felipe.donoso.cordero29@gmail.com>
2022-03-12 08:32:18 -0300

    format fixes

--- utils/number.js
+++ utils/number.js
@@ -1,11 +1,8 @@
 const isPrimeNumber = (num) => {
-  // quitar
   if (num < 2) return false;
-
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
-
   return true;
 };
 
